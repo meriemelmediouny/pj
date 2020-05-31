fe2dc8608404860e04415840f0a00e750f50e4be
Fatima-ezzahra <fatimaezzahrazahid7@gmail.com>
2020-05-31 23:58:50 +0100

    add js

---
+++ projet orientation scolaire/Varsity/assets/js/script.js
@@ -0,0 +1,16 @@
+
+
+function myFunction() { 
+    let input = document.getElementById('myInput').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByClassName('test'); 
+      
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="list-item";                  
+        } 
+    } 
+} 
